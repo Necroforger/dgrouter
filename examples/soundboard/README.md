8d1d399cd739769bc7ae6dd1870f30ddc191db33
necro <necroforger@gmail.com>
2018-07-21 06:23:31 -0400

    Add music player example

---
+++ examples/soundboard/README.md
@@ -0,0 +1,26 @@
+# soundboard
+Example music bot using dca-rs to stream and transcode music
+- Responds to mentions
+- Plays music from youtube
+
+See the sounds directory for an example of how to include some media files.
+If the file extension ends in .json, the bot will create a command that will search youtube with the given parameters. The json should be in the form of
+```json
+[
+	["name", "youtube-url"],
+	["name2", "youtube-url"]
+]
+```
+
+## Dependencies
+This bot depends on [ffmpeg](ffmpeg.org) to transcode the audio to opus.
+It should be installed to your path.
+
+[Here is a guide for installing on windows](https://github.com/adaptlearning/adapt_authoring/wiki/Installing-FFmpeg)
+
+## Flags
+
+| Flag | description |
+|------|-------------|
+| t    | bot token   |
+| p    | bot prefix  |
---
+++ examples/soundboard/soundboard.go
@@ -0,0 +1,303 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strconv"
+	"strings"
+
+	"github.com/rylio/ytdl"
+
+	"github.com/jonas747/dca"
+
+	"sync"
+
+	"github.com/Necroforger/dgrouter/exrouter"
+	"github.com/bwmarrin/discordgo"
+)
+
+// Command line flags
+var (
+	fToken    = flag.String("t", "", "bot token")
+	fPrefix   = flag.String("p", "!", "bot prefix")
+	fSoundDir = flag.String("d", "sounds", "directory of sound files")
+)
+
+func decodeFromFile(path string, v interface{}) error {
+	f, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	return json.NewDecoder(f).Decode(&v)
+}
+
+func trimExtension(p string) string {
+	return strings.TrimSuffix(p, filepath.Ext(p))
+}
+
+func main() {
+	flag.Parse()
+
+	s, err := discordgo.New(*fToken)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	router := exrouter.New()
+
+	// Create playback functions
+	files, err := ioutil.ReadDir(*fSoundDir)
+	if err != nil {
+		log.Fatal("no sound directory: ", err)
+	}
+	for _, v := range files {
+		if v.IsDir() {
+			continue
+		}
+		if filepath.Ext(v.Name()) == ".json" {
+			var data = [][]string{}
+			decodeFromFile(path.Join(*fSoundDir, v.Name()), &data)
+			for _, d := range data {
+				router.On(trimExtension(d[0]), createYoutubeFunction(d[1])).Desc("Plays " + d[1])
+			}
+			continue
+		}
+		router.On(
+			strings.TrimSuffix(
+				v.Name(),
+				path.Ext(v.Name()),
+			),
+			createMusicFunction(
+				filepath.Join(*fSoundDir, v.Name()),
+			),
+		).Desc("plays " + v.Name())
+	}
+
+	router.On("stop", func(ctx *exrouter.Context) {
+		s.Lock()
+		if vc, ok := s.VoiceConnections[ctx.Msg.GuildID]; ok {
+			if v, ok := streams.Get(ctx.Msg.GuildID); ok {
+				v.(*streamSession).EncodeSession.Stop() // Stop the encode session if it is running
+			}
+			go vc.Disconnect()
+		}
+		s.Unlock()
+	}).Desc("Stops the currently running stream")
+
+	helpRoute := router.On("help", func(ctx *exrouter.Context) {
+		var text = ""
+		var maxlen int
+		for _, v := range router.Routes {
+			if len(v.Name) > maxlen {
+				maxlen = len(v.Name)
+			}
+		}
+		for _, v := range router.Routes {
+			text += fmt.Sprintf("%-"+strconv.Itoa(maxlen+5)+"s:    %s\n", v.Name, v.Description)
+		}
+		ctx.Reply("```" + text + "```")
+	}).Desc("prints this help menu")
+
+	// Add message handler
+	s.AddHandler(func(_ *discordgo.Session, m *discordgo.MessageCreate) {
+		router.FindAndExecute(s, *fPrefix, s.State.User.ID, m.Message, helpRoute)
+	})
+
+	err = s.Open()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("bot is running...")
+	// Prevent the bot from exiting
+	<-make(chan struct{})
+}
+
+// thread safe map
+type syncmap struct {
+	sync.RWMutex
+	Data map[string]interface{}
+}
+
+func newSyncmap() *syncmap {
+	return &syncmap{
+		Data: map[string]interface{}{},
+	}
+}
+
+// Set ...
+func (s *syncmap) Set(key string, value interface{}) {
+	s.Lock()
+	s.Data[key] = value
+	s.Unlock()
+}
+
+// Get ...
+func (s *syncmap) Get(key string) (interface{}, bool) {
+	s.RLock()
+	defer s.RUnlock()
+	if v, ok := s.Data[key]; ok {
+		return v, true
+	}
+	return nil, false
+}
+
+// links guild ids to their dca encoder session
+var streams = newSyncmap()
+
+type streamSession struct {
+	EncodeSession *dca.EncodeSession
+	StreamSession *dca.StreamingSession
+}
+
+func createMusicFunction(fpath string) func(ctx *exrouter.Context) {
+	return func(ctx *exrouter.Context) {
+		vc, err := getVoiceConnection(ctx.Ses, ctx.Msg.Author.ID, ctx.Msg.GuildID)
+		if err != nil {
+			ctx.Reply("error obtaining voice connection")
+			log.Println("error getting voice connection: ", err)
+			return
+		}
+
+		log.Println("Creating encoding session")
+
+		opts := dca.StdEncodeOptions
+		opts.RawOutput = true
+		opts.Bitrate = 120
+
+		encodeSession, err := dca.EncodeFile(fpath, opts)
+		if err != nil {
+			ctx.Reply("error creating encode session")
+			log.Println("error creating encode session: ", err)
+			return
+		}
+
+		// set speaking to true
+		if err := vc.Speaking(true); err != nil {
+			ctx.Reply("could not set speaking to true")
+			return
+		}
+		defer vc.Speaking(false)
+
+		done := make(chan error)
+		streamer := dca.NewStream(encodeSession, vc, done)
+
+		// Stop any currently running stream
+		if v, ok := streams.Get(ctx.Msg.GuildID); ok {
+			log.Println("stopping existing streamer")
+			v.(*streamSession).EncodeSession.Stop()
+			v.(*streamSession).EncodeSession.Cleanup()
+		}
+
+		log.Println("Adding streaming to map")
+		streams.Set(ctx.Msg.GuildID, &streamSession{
+			EncodeSession: encodeSession,
+			StreamSession: streamer,
+		})
+
+		if err := <-done; err != nil {
+			log.Println(err)
+			// Clean up incase something happened and ffmpeg is still running
+			encodeSession.Truncate()
+		}
+	}
+}
+
+// createYoutubeFunction creates a function for streaming youtube videos
+func createYoutubeFunction(yurl string) func(ctx *exrouter.Context) {
+	return func(ctx *exrouter.Context) {
+		vc, err := getVoiceConnection(ctx.Ses, ctx.Msg.Author.ID, ctx.Msg.GuildID)
+		if err != nil {
+			ctx.Reply("error obtaining voice connection")
+			log.Println("error getting voice connection: ", err)
+			return
+		}
+
+		err = playYoutubeVideo(vc, yurl)
+		if err != nil {
+			ctx.Reply("error playing youtube video: ", err)
+		}
+	}
+}
+
+func playYoutubeVideo(vc *discordgo.VoiceConnection, yurl string) error {
+	info, err := ytdl.GetVideoInfo(yurl)
+	if err != nil {
+		return err
+	}
+
+	rd, wr := io.Pipe()
+	go func() {
+		err := info.Download(info.Formats.Best(ytdl.FormatAudioEncodingKey)[0], wr)
+		if err != nil {
+			log.Println(err)
+		}
+		wr.Close()
+	}()
+
+	opts := dca.StdEncodeOptions
+	opts.RawOutput = true
+	opts.Bitrate = 120
+
+	encodeSession, err := dca.EncodeMem(rd, opts)
+	if err != nil {
+		return err
+	}
+
+	// set speaking to true
+	if err := vc.Speaking(true); err != nil {
+		return err
+	}
+	defer vc.Speaking(false)
+
+	done := make(chan error)
+	streamer := dca.NewStream(encodeSession, vc, done)
+
+	// Stop any currently running stream
+	if v, ok := streams.Get(vc.GuildID); ok {
+		log.Println("stopping existing streamer")
+		v.(*streamSession).EncodeSession.Stop()
+		v.(*streamSession).EncodeSession.Cleanup()
+	}
+
+	log.Println("Adding streaming to map")
+	streams.Set(vc.GuildID, &streamSession{
+		EncodeSession: encodeSession,
+		StreamSession: streamer,
+	})
+
+	if err := <-done; err != nil {
+		log.Println(err)
+		// Clean up incase something happened and ffmpeg is still running
+		encodeSession.Truncate()
+	}
+
+	return nil
+}
+
+// getVoiceConnection gets a bot's voice connection
+func getVoiceConnection(s *discordgo.Session, userID string, guildID string) (*discordgo.VoiceConnection, error) {
+	guild, err := s.State.Guild(guildID)
+	if err != nil {
+		guild, err = s.Guild(guildID)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	for _, v := range guild.VoiceStates {
+		if v.UserID == userID {
+			return s.ChannelVoiceJoin(guildID, v.ChannelID, false, false)
+		}
+	}
+
+	return nil, errors.New("Voice connection not found")
+}
